9a91a472d69cf1c8218531b108dab391632cc3fe
barbishop <vladimir.a.fedorenko@gmail.com>
2024-05-09 16:57:27 +0300

    task 4/5/6 +css

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const spanColor = document.querySelector('.color');
+
+function formHandler(event) {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  spanColor.textContent = color;
+}
+
+button.addEventListener('click', formHandler);
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,45 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector('input');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const divCont = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  divCont.innerHTML = '';
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    div.classList.add('box');
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+
+    divCont.appendChild(div);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  divCont.innerHTML = '';
+}
+
+btnCreate.addEventListener('click', handleCreate);
+
+function handleCreate() {
+  const amount = Number(inputEl.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    inputEl.value = '';
+  } else {
+    alert('Please enter a number between 1 and 100.');
+  }
+}
+
+btnDestroy.addEventListener('click', handleDestroy);
+
+function handleDestroy() {
+  destroyBoxes();
+}
